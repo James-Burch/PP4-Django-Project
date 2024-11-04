67a3d78446a5c0a8833fa7fd9eed65d8d0d5ff55
James Burch <burchjames6@gmail.com>
2024-11-04 13:52:53 +0000

    Add login required to book

--- bookapp/views.py
+++ bookapp/views.py
@@ -3,15 +3,17 @@ from .models import Booking, AvailableTime, Meta
 from django.utils import timezone
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import inlineformset_factory
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def home(request):
     return render(request, "home.html")
 
+@login_required
 def booking(request):
     return render(request, "bookingpage.html")
 
 def register(request):
     form = UserCreationForm()
     context = {'form':form}
-    return render(request, "register.html")
+    return render(request, "register.html", context)
