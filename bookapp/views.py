05065ceee7bd0491066292c7d8f69528a3770614
James Burch <burchjames6@gmail.com>
2024-11-11 11:02:02 +0000

    Add booking views

--- bookapp/views.py
+++ bookapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
-from .models import Booking, AvailableTime, Meta
+from .models import Booking, AvailableDate
 from django.utils import timezone
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import inlineformset_factory
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import BookingForm
+
 # Create your views here.
 
 def home(request):
@@ -17,3 +20,21 @@ def register(request):
     form = UserCreationForm()
     context = {'form':form}
     return render(request, "register.html", context)
+
+@login_required
+def create_booking(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.user = request.user
+            # Check if slot is already booked
+            if Booking.objects.filter(date=booking.date, time_slot=booking.time_slot).exists():
+                messages.error(request, "This time slot is already booked. Please choose another.")
+            else:
+                booking.save()
+                messages.success(request, "Your booking was successful!")
+                return redirect('booking_confirmation')
+    else:
+        form = BookingForm()
+    return render(request, 'bookingpage.html', {'form': form})
