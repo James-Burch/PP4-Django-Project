835a69fb9f7ae778ba4743b54bc4097be87720c6
James Burch <burchjames6@gmail.com>
2024-11-18 22:04:59 +0000

    Change booking form to have date and time drop downs

--- bookapp/admin.py
+++ bookapp/admin.py
@@ -1,11 +1,3 @@
-# from django.contrib import admin
-# from django import forms
-# from .models import Booking, AvailableDate
-
-# # Register your models here.
-# admin.site.register(AvailableDate)
-# admin.site.register(Booking)
-
 from django.contrib import admin
 from .models import Booking, Player
 
--- bookapp/forms.py
+++ bookapp/forms.py
@@ -5,6 +5,20 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['date', 'time']
+        widgets = {
+            'date': forms.SelectDateWidget(),  # Provides a dropdown
+            'time': forms.Select(choices=[
+                ('09:00:00', '9:00 AM'),
+                ('10:00:00', '10:00 AM'),
+                ('11:00:00', '11:00 AM'),
+                ('12:00:00', '12:00 PM'),
+                ('13:00:00', '1:00 PM'),
+                ('14:00:00', '2:00 PM'),
+                ('15:00:00', '3:00 PM'),
+                ('16:00:00', '4:00 PM'),
+            ]),
+        }
+
 
 class PlayerForm(forms.ModelForm):
     class Meta:
