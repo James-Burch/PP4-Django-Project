a3b545d24d4f080a662d3e0c2186a7517fc03847
James Burch <burchjames6@gmail.com>
2024-11-18 17:43:24 +0000

    Validate views.py code using pep8 add validated code back

--- bookapp/views.py
+++ bookapp/views.py
@@ -4,25 +4,34 @@ from .forms import BookingForm, PlayerForm
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
+
 def home(request):
     return render(request, "home.html")
+
 
 @login_required
 def booking(request):
     form = BookingForm()
     return render(request, "bookingpage.html", {'form': form})
 
+
 def booking_confirmation(request):
     return render(request, 'booking_confirmation.html')
 
+
 def register(request):
     form = UserCreationForm()
-    context = {'form':form}
-    messages.success('You have successfully created an account! You can now make bookings!')
+    context = {'form': form}
+    messages.success(
+        request,
+        'You have successfully created an account! You can now make bookings!'
+    )
     return render(request, "register.html", context)
+
 
 @login_required
 def booking_view(request):
@@ -36,14 +45,24 @@ def booking_view(request):
             player = player_form.save(commit=False)
             player.booking = booking
             player.save()
-            messages.success(request, 'Your booking has been successfully created!')
+            messages.success(
+                request,
+                'Your booking has been successfully created!'
+            )
             return redirect('my_bookings')
         else:
-            messages.error(request, '! There was an error with your booking. Please check the booking instructions and try again. !')    
+            messages.error(
+                request,
+                'There was an error with your booking. '
+                'Please check the booking instructions and try again.'
+            )
     else:
         booking_form = BookingForm()
         player_form = PlayerForm()
-    return render(request, 'bookingpage.html', {'booking_form': booking_form, 'player_form': player_form})
+    return render(request, 'bookingpage.html', {
+        'booking_form': booking_form,
+        'player_form': player_form
+    })
 
 
 @login_required
@@ -51,26 +70,33 @@ def my_bookings(request):
     bookings = Booking.objects.filter(user=request.user)
     return render(request, 'my_bookings.html', {'bookings': bookings})
 
+
 @login_required
 def edit_booking(request, booking_id):
     booking = get_object_or_404(Booking, id=booking_id, user=request.user)
-    
+
     if request.method == 'POST':
         if 'save_changes' in request.POST:
             booking_form = BookingForm(request.POST, instance=booking)
             if booking_form.is_valid():
                 booking_form.save()
-            messages.success(request, 'Your booking has been successfully updated!')    
-            return redirect('my_bookings')
-        
+                messages.success(
+                    request,
+                    'Your booking has been successfully updated!'
+                )
+                return redirect('my_bookings')
+
         elif 'delete_booking' in request.POST:
             booking.delete()
-            messages.success(request, 'Your booking has been successfully cancelled!')
+            messages.success(
+                request,
+                'Your booking has been successfully cancelled!'
+            )
             return redirect('my_bookings')
-    
+
     else:
         booking_form = BookingForm(instance=booking)
-        player_form = PlayerForm()  
+        player_form = PlayerForm()
 
     return render(request, 'edit_booking.html', {
         'booking_form': booking_form,
